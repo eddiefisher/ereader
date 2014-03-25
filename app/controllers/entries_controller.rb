12171a3672aed8d8a42a372bd41c807507601401
eddiefisher <eddifisher@bk.ru>
2014-03-26 02:20:04 +0700

    first style

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,15 @@
 class EntriesController < ApplicationController
+  before_filter :entry
+
+  def index
+    @entries = Entry.all
+  end
+
+  def show    
+  end
+
+  private
+  def entry
+    @entry = Entry.find(params[:id]) if params[:id]
+  end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,6 @@
 class Entry < ActiveRecord::Base
+  default_scope ->{ order(published_at: :desc) }
+
   def self.update_from_feed(feed_url)
     feed = Feedjira::Feed.fetch_and_parse(feed_url)
     add_entries(feed.entries)
@@ -16,5 +18,15 @@ class Entry < ActiveRecord::Base
         :guid         => entry.id
       ) unless exists?(:guid => entry.id)
     end
+  end
+
+  def self.get_body
+    source = open(params[:url], "User-Agent" => "Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25").read
+    @results = Readability::Document.new( source,
+                                          tags: %w[div p img a h1 h2 h3 h4 h5 h6 strong code pre span b i blockquote ul ol li dd dt],
+                                          attributes: %w[src href class],
+                                          remove_empty_nodes: false,
+                                          remove_unlikely_candidates: false)
+    @results.content
   end
 end
