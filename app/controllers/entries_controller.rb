9d49c6d34b72f88b49a5274112d82e3df801007d
eddiefisher <eddifisher@bk.ru>
2014-04-14 20:15:53 +0700

    show 40 entries in channel list

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,7 +4,7 @@ class EntriesController < ApplicationController
 
   def index
     if params.fetch(:channel_id, false)
-      @entries = Entry.includes(:channel).where(channel_id: params[:channel_id]).limit(20)
+      @entries = Entry.includes(:channel).where(channel_id: params[:channel_id]).limit(40)
     else
       @entries = Entry.includes(:channel).where('entries.published_at > ?', 1.day.ago)
     end
