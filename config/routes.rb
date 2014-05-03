fcb2140775903e24d93ebe64daad2adb5b8ef170
eddiefisher <eddifisher@bk.ru>
2014-05-03 14:57:10 +0700

    added menu starred and flagged

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     collection do
       get 'channel/:channel_id', controller: 'entries', action: 'index', as: 'channel'
       get 'get_body', action: 'get_body', as: 'get_body'
+      get 'flaged', action: 'flaged', as: 'flaged'
+      get 'stared', action: 'stared', as: 'stared'
       post "action/:id/:method", action: "action", as: "action"
       post "batch_actions", action: "batch_actions", as: "batch_actions"
     end
